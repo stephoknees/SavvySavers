009631d04e2d48d42bfb66b2647c9eb405ae99a6
stephoknees <stephctran@utexas.edu>
2019-10-26 17:32:15 -0400

    working with local storage

--- popup.js
+++ popup.js
@@ -4,28 +4,40 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
     }
   });
   
-function scrapePage() {
-    var message = document.querySelector('#message');
-
-    chrome.tabs.executeScript(null, {
-        file: "getPagesSource.js"
-    }, function() {
-        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
-        if (chrome.runtime.lastError) {
-          message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-        }
-        let text = message.innerText;
-        let prepos = text.lastIndexOf("tax");
-        prepos = text.indexOf("total", prepos);
-        let pos = text.indexOf("$", prepos);
-        pos += 1;
-        let amount = '';
-        while(text.charAt(pos) != '<') {
-            amount += text.charAt(pos++);
-        }
-        message.innerText = amount;
+let scrapePage = 
+  chrome.storage.local.get({items:[]}, function(result) {
+    // gets list of pairs
+    let items = result.items;
+    chrome.tabs.getSelected(null, function(tab) {
+      let domain = tab.url;
+      var message = document.querySelector('#message');
+      chrome.tabs.executeScript(null, {
+          file: "getPagesSource.js"
+      }, function() {
+          // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+          if (chrome.runtime.lastError) {
+            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+          }
+          let text = message.innerText;
+          let prepos = text.lastIndexOf("tax");
+          if(prepos < 0) return;
+          prepos = text.indexOf("total", prepos);
+          if(prepos < 0) return;
+          let pos = text.indexOf("$", prepos);
+          if(pos < 0) return;
+          pos += 1;
+          let amount = '';
+          while(text.charAt(pos) != '<') {
+              amount += text.charAt(pos++);
+          }
+          let item = {}
+          item[domain] = amount;
+          items.push(item);
+          chrome.storage.local.set({items: items});
+          //alert(items);
+          chrome.storage.local.clear();
+      });
     });
-
-}
+  });
   
-  window.onload = scrapePage;
+window.onload = scrapePage;
