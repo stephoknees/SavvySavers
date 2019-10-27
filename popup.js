a084270d35bec9f4e9ebba2c67e5610cce567647
Sandeep Singh <serperior2@gmail.com>
2019-10-27 01:13:38 -0400

    Style and functionality

--- popup.js
+++ popup.js
@@ -1,52 +1,91 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
-    if (request.action == "getSource") {
-      message.innerText = request.source;
-    }
-  });
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
   
 function scrapePage() {
   chrome.storage.local.get({items:[]}, function(result) {
     // gets list of pairs
     let items = result.items;
-    console.log(items);
+    
     chrome.tabs.getSelected(null, function(tab) {
       let domain = tab.url;
+
       let begin = domain.indexOf("www.") + 4;
       let begin2 = domain.indexOf("https://") + 8;
       begin = (begin2 > begin) ? begin2 : begin;
       let end = domain.indexOf(".com");
       domain = domain.substring(begin,end);
-      let message = document.querySelector('#message');
+
+      var message = document.querySelector('#message');
+      
       chrome.tabs.executeScript(null, {
           file: "getPagesSource.js"
       }, function() {
-          // If you try and inject into an extensions page or the webstore/NTP get an error
-          if (chrome.runtime.lastError) {
-            message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-          }
-          let text = message.innerText.toLowerCase();
-          let prepos = text.lastIndexOf("tax");
-          if(prepos < 0) return;
-          prepos = text.indexOf("total", prepos);
-          if(prepos < 0) return;
-          let pos = text.indexOf("$", prepos);
-          if(pos < 0) return;
-          pos += 1;
-          let amount = '';
-          while(text.charAt(pos) != '<') {
-              amount += text.charAt(pos++);
-          }
-          let item = {}
-          item[domain] = amount.trim();
-          items.push(item);
-          chrome.storage.local.set({items: items});
-          //alert(items);
-          console.log(items);
-          //chrome.storage.local.clear();
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError) {
+          message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+        }
+        
+        let text = message.innerText.toLowerCase();
+        let prepos = text.lastIndexOf("tax");
+        if(prepos < 0) return;
+        
+        prepos = text.lastIndexOf("total", prepos);
+        if(prepos < 0) return;
+        
+        let pos = text.indexOf("$", prepos);
+        if(pos < 0) return;
+        
+        pos += 1;
+        let amount = '';
+        
+        while(text.charAt(pos) != '<') {
+            amount += text.charAt(pos++);
+        }
+        
+        let item = {}
+        item[domain] = amount.trim();
+        items.push(item);
+        chrome.storage.local.set({items: items});
+        // chrome.storage.local.clear();
+
+        displayPurchase(items[0]);
       });
     });
   });
 }
-// links button click
-let purchase = document.getElementsByClassName("purchase_button");
-purchase[0].onclick = scrapePage;
+  
+function displayPurchase(purchases) {
+  var purchaseContainer = document.getElementById("purchaseContainer");
+  var buttonContainer = document.createElement("div");
+  var purchaseEntry = document.createElement("p");
+  var purchaseDelete = document.createElement("button");
+  purchaseDelete.innerHTML = "x";
+
+  for (data in purchases) {
+    purchaseEntry.innerHTML = `${data}: ${purchases[data]}`;
+
+    buttonContainer.classList.add("buttonContainer");
+
+    purchaseDelete.addEventListener("click", function() {
+      deleteEntry(purchaseDelete);
+    });
+    purchaseDelete.classList.add("delete");
+    buttonContainer.appendChild(purchaseDelete);
+    
+    purchaseEntry.classList.add("entry");
+    buttonContainer.appendChild(purchaseEntry);
+    
+    purchaseContainer.appendChild(buttonContainer);
+  }
+}
+
+function deleteEntry(entry) {
+  console.log(entry.parentNode);
+  entry.parentNode.remove();
+}
+
+var addButton = document.getElementById("add");
+addButton.addEventListener("click", scrapePage);
